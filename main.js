9b3c1cf5d1cfce6a09dcd1dbb16ebc866b838dfa
Jing Kang lee <leejingkang6@gmail.com>
2021-06-19 14:25:04 +0800

    test update

---
+++ main.js
@@ -0,0 +1,39 @@
+import './style.css'
+
+import * as THREE from 'three';
+
+//Setup
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio); 
+renderer.setSize( window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10,3, 16,100);
+const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe:true});
+const torus = new THREE.Mesh( geometry, material ); // combining geometry and material
+
+scene.add(torus);
+
+function animate(){
+  // we don't want to call the render method over and over in the code, 
+  // so a better approach is to just use a revursive function
+  requestAnimationFrame(animate);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+
+  renderer.render(scene, camera);
+}
+animate();
+
