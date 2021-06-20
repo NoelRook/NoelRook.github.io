c7aaad5d8740d2d86632119bb04bf93e77d6f021
Jing Kang lee <leejingkang6@gmail.com>
2021-06-20 11:41:53 +0800

    new commit

--- main.js
+++ main.js
@@ -1,52 +1,38 @@
-import * as THREE from './three/build/three.js';
 
-import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js';
+var camera, scene, renderer;
+var geometry, material, mesh;
 
-//Setup
+	init();
+	animate();
 
-const scene = new THREE.Scene();
+	function init() {
 
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1, 1000);
+		camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+		camera.position.z = 1000;
 
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
-});
+		scene = new THREE.Scene();
 
-renderer.setPixelRatio(window.devicePixelRatio); 
-renderer.setSize( window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+		geometry = new THREE.CubeGeometry( 200, 200, 200 );
+		material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
 
-renderer.render(scene, camera);
+		mesh = new THREE.Mesh( geometry, material );
+		scene.add( mesh );
 
-const geometry = new THREE.TorusGeometry(10,3, 16,100);
-const material = new THREE.MeshStandardMaterial({ color: 0xFF6347});
-const torus = new THREE.Mesh( geometry, material ); // combining geometry and material
+		renderer = new THREE.CanvasRenderer();
+		renderer.setSize( window.innerWidth, window.innerHeight );
 
-scene.add(torus);
+		document.body.appendChild( renderer.domElement );
 
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(20,20,20);
+	}
 
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight);
+	function animate() {
 
-const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper,gridHelper);
+		// note: three.js includes requestAnimationFrame shim
+		requestAnimationFrame( animate );
 
-const controls = new OrbitControls(camera, renderer.domElement);
+		mesh.rotation.x += 0.01;
+		mesh.rotation.y += 0.02;
 
-function animate(){
-  // we don't want to call the render method over and over in the code, 
-  // so a better approach is to just use a revursive function
-  requestAnimationFrame(animate);
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
+		renderer.render( scene, camera );
 
-  controls.update();
-
-  renderer.render(scene, camera);
-}
-animate();
-
+	}
